6f068711a244a602a966f52e82933c322ef89cae
Basit Khokhar <basitkhokhar957@gmail.com>
2024-03-14 21:33:54 -0700

    basit

---
+++ public/js/javascript.js
@@ -0,0 +1,78 @@
+
+// list display function
+function dashboardList() {
+    var list1 = document.getElementById("dashboard-list");
+    if (list1.style.display === "none") {
+        list1.style.display = "block";
+    } else {
+        list1.style.display = "none";
+    }
+}
+function secondList() {
+    var list2 = document.getElementById("a2");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function eventList() {
+    var list2 = document.getElementById("a3");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function ecommerceList() {
+    var list2 = document.getElementById("a4");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function elearningList() {
+    var list2 = document.getElementById("a5");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function socialList() {
+    var list2 = document.getElementById("a6");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+function supportdeskList() {
+    var list2 = document.getElementById("a7");
+    if (list2.style.display === "none") {
+        list2.style.display = "block";
+    } else {
+        list2.style.display = "none";
+    }
+}
+
+//  for complete side bar dsiplay//
+function Mainlist() {
+    var mainlist = document.getElementById("A1");
+    var rightside=document.getElementById("rightside");
+    if (mainlist.style.display === "none"  ) {
+        mainlist.style.display = "block";
+    } else {
+        mainlist.style.display = "none";
+    }
+}
+// function width(){
+//     var rightside=document.getElementById("rightside");
+//     if (rightside.style.marginLeft === "36.3rem") {
+//         rightside.style.marginLeft="0" && rightside.style.width=="100%";
+//     } else {
+//         rightside.style.marginLeft="36.3rem";
+//     }
+// }
+
